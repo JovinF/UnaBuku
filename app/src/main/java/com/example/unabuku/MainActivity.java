7f2f1847b8e28079eb2f85cd2af92e8418c89965
Jovin <frans541@gmail.com>
2020-03-09 12:09:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/unabuku/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.unabuku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+Handler handler;
+Runnable runnable;
+ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.img);
+        img.animate().alpha(3000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashScreen = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(splashScreen);
+                finish();
+            }
+        },4000);
+    }
+}
