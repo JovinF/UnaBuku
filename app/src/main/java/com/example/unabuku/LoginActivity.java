253af0f7e2d04be3e6111ae10356197c35fe8203
Jovin <frans541@gmail.com>
2020-03-11 08:39:59 -0300

    BottomNavigation added Fragments added

--- app/src/main/java/com/example/unabuku/LoginActivity.java
+++ app/src/main/java/com/example/unabuku/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.unabuku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.login_user);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+
+                Intent menuIntent = new Intent(LoginActivity.this,MenuActivity.class);
+            startActivity(menuIntent);
+            finish();
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/unabuku/MenuActivity.java
@@ -0,0 +1,49 @@
+package com.example.unabuku;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MenuActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+//    private static final String TAG = "MenuActivity";
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    OrdersFragment ordersFragment = new OrdersFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+switch (item.getItemId()){
+    case R.id.navigation_home:
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        return true;
+    case R.id.navigation_orders:
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,ordersFragment).commit();
+        return true;
+    case R.id.navigation_profile:
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+        return true;
+}
+        return false;
+    }
+}
